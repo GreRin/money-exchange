a49cc179f7a96488426a6cea0b937fd79b4f0431
GreRin <rigor1988@gmail.com>
2019-03-24 22:13:40 +0300

    feat: solve money-exchange

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ module.exports = function makeExchange(currency) {
 	// 	return ({ error: "You are rich, my friend! We don't have so much coins for exchange" })
 	// }
 	// if (amount === 0) return currency = {}
-
+	// if (amount < 0) return currency = {}
 	// if (amount <= 1000) {
 	// 	for (let i; i < coins.length; i++) {
 	// 		if ((currency % coins[0]) === 0) {
@@ -22,39 +22,53 @@ module.exports = function makeExchange(currency) {
 	// 		}
 			
 	// 	}
-		// if ((currency % coins[0]) === 0) {result = {"H": currency / coins[0]}}
-		// if ((currency - (currency / coins[0])  % coins[1]) === 0) {result = {"H": currency / coins[0], "Q": }}	
+	// 	if ((currency % coins[0]) === 0) {result = {"H": currency / coins[0]}}
+	// 	if ((currency - (currency / coins[0])  % coins[1]) === 0) {result = {"H": currency / coins[0], "Q": }}	
 	// 	return result
 	// }
 		// Return an object containing the minimum number of coins needed to make change
-	let valH = 0,	valQ = 0,	valD = 0,	valN = 0,	valP = 0;
-	switch (true) {
-			case (currency === 0):
-			return {}
-			case (currency < 0):
+
+    switch (true) {
+			case (currency <= 0):
 			return {}
 			case (currency > 10000):
 			return { error: 'You are rich, my friend! We don\'t have so much coins for exchange' }
-			case (currency > 1001 && currency <= 10000):
+			case (currency > 0 && currency <= 10000):
+			let arr = {},
+					valH = 0,
+					valQ = 0,
+					valD = 0,
+					valN = 0,
+					valP = 0;
+			const H = 'H',
+					Q = 'Q',
+					D = 'D',
+					N = 'N',
+					P = 'P';
 			while (currency > 0) {
 					if (currency / 50 >= 1) {
 							currency -= 50
 							valH++
+							arr[H] = valH
 					} else if (currency / 25 >= 1) {
 							currency -= 25
 							valQ++
+							arr[Q] = valQ
 					} else if (currency / 10 >= 1) {
 							currency -= 10
 							valD++
+							arr[D] = valD
 					} else if (currency / 5 >= 1) {
 							currency -= 5
 							valN++
+							arr[N] = valN
 					} else if (currency > 0) {
 							currency -= 1
 							valP++
+							arr[P] = valP
 					}
 			}
-			return { H: valH, Q: valQ, D: valD, N: valN, P: valP }
+			return arr
 			default:
 					break;
 	}
